9b9179ebaf570eff15f012496347e1fb6e7cab37
Jéssica Thomaz <jessica.claudiat@gmail.com>
2022-11-29 11:29:00 -0300

    Criação da Interface IPersonService

---
+++ RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+
+    private readonly ILogger<PersonController> _logger;
+
+    public PersonController(ILogger<PersonController> logger)
+    {
+        _logger = logger;
+    }
+
+    //SOMA
+    [HttpGet("sum/{firstNumber}/{secondNumber}")]
+    public IActionResult Sum(string firstNumber, string secondNumber)
+    {
+        return BadRequest("Invalid Input");
+    }
+}
---
+++ RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,12 @@
+﻿using RestWithASPNET.Controllers.Model;
+using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services {
+    public interface IPersonService {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
